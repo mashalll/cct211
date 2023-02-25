bd03ba10c5755f3071790d48e15cb91d02fb8ab6
Cristinalopera <126193298+Cristinalopera@users.noreply.github.com>
2023-02-25 17:50:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
-
-print("hello world")
+from pygame.locals import *
+pygame.init()
+width = 900
+height = 400
+window = pygame.display.set_mode((width,height))
+background = pygame.image.load('background.png')
+background = pygame.transform.scale(background,(width,height))
+ 
+ 
+runing = True
+while runing:
+    window.fill((0,0,0))
+    window.blit(background,(0,-1))
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            runing = False
+    pygame.display.update()
+pygame.quit()
